1b589bce2937c211724c92b8e8630995077a3edf
hwu5542 <hwu5542@gmail.com>
2021-08-11 20:26:08 -0500

    get all profiles

--- src/main/java/com/revature/repo/ProfileDAOImpl.java
+++ src/main/java/com/revature/repo/ProfileDAOImpl.java
@@ -1,13 +1,30 @@
 package com.revature.repo;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.revature.models.Profile;
+
 @Repository
 @Transactional
 public class ProfileDAOImpl implements ProfileDAO {
+	private SessionFactory sessionFactory;
+	
 	@Autowired
-	private SessionFactory sf;
+	public ProfileDAOImpl(SessionFactory sessionFactory) {
+		super();
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Override
+	public List<Profile> getAllProfile() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		return session.createQuery("FROM profile").list();
+	}
 }
