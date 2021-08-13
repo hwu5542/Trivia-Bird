5b7d599e4c95de7324f9928b0844dae118e569db
rblack <blackrmb@yahoo.com>
2021-08-13 14:41:10 -0400

    Logging in and Adding Profile

--- src/main/java/com/revature/controllers/ProfileController.java
+++ src/main/java/com/revature/controllers/ProfileController.java
@@ -3,8 +3,13 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +35,22 @@ public class ProfileController {
 		return profileService.getProfiles();
 	}
 	
+	@GetMapping("/{username}/{password}")
+	public ResponseEntity<Profile> logIn(@PathVariable("username") String username, @PathVariable("password") String password) {
+		Profile profile = profileService.logIn(username, password);
+		if(profile==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		else
+			return ResponseEntity.status(HttpStatus.OK).body(profile);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Profile> addProfile(@RequestBody Profile profile){
+		String result = profileService.addProfile(profile);
+		if(result.compareTo("Profile successfully added") == 0)
+			return ResponseEntity.status(HttpStatus.OK).build();
+		else
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
 }
