b603b8f7fbeb30a7c3c5967a07097f44ea0e5ab4
hwu5542 <hwu5542@gmail.com>
2021-08-15 14:25:11 -0500

    fix query

--- src/main/java/com/revature/models/Profile.java
+++ src/main/java/com/revature/models/Profile.java
@@ -7,9 +7,8 @@ import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 @Entity
-@Table(name="Profile")
 public class Profile {
 	@Id
 	private String username;
